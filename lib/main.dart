0b9c2d27e5748ed7855c158c87f453cfd99b2b35
gusenga thierry <t.gusenga@alustudent.com>
2026-03-14 21:06:48 +0200

    updating the spalshscreen to transiton to login
    Co-Authored-By: Betelhem <b.chelebo@alustudent.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:organic_food_directory/screens/splash_screen.dart';
+import 'package:organic_food_directory/screens/login_screen.dart';
 import 'package:organic_food_directory/screens/main_screen.dart';
 import 'package:organic_food_directory/screens/home_page.dart';
-import 'package:organic_food_directory/screens/search_results_page.dart';
 import 'package:organic_food_directory/screens/category_page.dart';
 import 'package:organic_food_directory/screens/product_details_page.dart';
+import 'package:organic_food_directory/screens/my_list_page.dart';
 import 'package:organic_food_directory/screens/profile_page.dart';
+import 'package:organic_food_directory/screens/external_link_page.dart';
+import 'package:organic_food_directory/screens/search_results_page.dart';
 import 'package:organic_food_directory/screens/edit_profile_page.dart';
 import 'package:organic_food_directory/screens/favorites_page.dart';
-import 'package:organic_food_directory/screens/my_list_page.dart';
-import 'package:organic_food_directory/screens/external_link_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,17 +28,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E7D32)),
         useMaterial3: true,
       ),
-      home: const MainScreen(),
+      initialRoute: '/',
       routes: {
-        '/home':           (context) => const MainScreen(),
+        '/': (context) => const SplashScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const MainScreen(),
+        '/category': (context) => const CategoryPage(),
+        '/product': (context) => const ProductDetailsPage(),
+        '/my-list': (context) => const MyListPage(),
+        '/profile': (context) => const ProfilePage(),
+        '/external-link': (context) => const ExternalLinkPage(),
         '/search-results': (context) => const SearchResultsPage(),
-        '/category':       (context) => const CategoryPage(),
-        '/product':        (context) => const ProductDetailsPage(),
-        '/profile':        (context) => const ProfilePage(),
-        '/edit-profile':   (context) => const EditProfilePage(),
-        '/favorites':      (context) => const FavoritesPage(),
-        '/my-list':        (context) => const MyListPage(),
-        '/external-link':  (context) => const ExternalLinkPage(),
+        '/edit-profile': (context) => const EditProfilePage(),
+        '/favorites': (context) => const FavoritesPage(),
       },
     );
   }
